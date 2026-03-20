983bdd38964aed37565d6c496abf528b3a63e78b
dylan2308 <dylangay0@gmail.com>
2026-03-20 10:30:30 +0800

    feat: Add recall and F2 to returned metrics dictionary

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -1,4 +1,5 @@
 import torch
+from sklearn.metrics import recall_score, fbeta_score
 
 
 def train_one_epoch(model, dataloader, criterion, optimizer, device):
@@ -7,6 +8,8 @@ def train_one_epoch(model, dataloader, criterion, optimizer, device):
     running_loss = 0.0
     running_correct = 0
     total_samples = 0
+    all_labels = []
+    all_preds = []
 
     for images, labels in dataloader:
         images = images.to(device)
@@ -26,12 +29,19 @@ def train_one_epoch(model, dataloader, criterion, optimizer, device):
         running_correct += (preds == labels).sum().item()
         total_samples += labels.size(0)
 
+        all_labels.extend(labels.squeeze(1).cpu().int().tolist())
+        all_preds.extend(preds.squeeze(1).cpu().int().tolist())
+
     epoch_loss = running_loss / total_samples
     epoch_acc = running_correct / total_samples
+    epoch_recall = recall_score(all_labels, all_preds, pos_label=1, zero_division=0)
+    epoch_f2 = fbeta_score(all_labels, all_preds, beta=2, pos_label=1, zero_division=0)
 
     return {
         "loss": epoch_loss,
-        "accuracy": epoch_acc
+        "accuracy": epoch_acc,
+        "recall": epoch_recall,
+        "f2": epoch_f2,
     }
 
 
@@ -42,6 +52,8 @@ def validate_one_epoch(model, dataloader, criterion, device):
     running_loss = 0.0
     running_correct = 0
     total_samples = 0
+    all_labels = []
+    all_preds = []
 
     for images, labels in dataloader:
         images = images.to(device)
@@ -56,10 +68,17 @@ def validate_one_epoch(model, dataloader, criterion, device):
         running_correct += (preds == labels).sum().item()
         total_samples += labels.size(0)
 
+        all_labels.extend(labels.squeeze(1).cpu().int().tolist())
+        all_preds.extend(preds.squeeze(1).cpu().int().tolist())
+
     epoch_loss = running_loss / total_samples
     epoch_acc = running_correct / total_samples
+    epoch_recall = recall_score(all_labels, all_preds, pos_label=1, zero_division=0)
+    epoch_f2 = fbeta_score(all_labels, all_preds, beta=2, pos_label=1, zero_division=0)
 
     return {
         "loss": epoch_loss,
-        "accuracy": epoch_acc
+        "accuracy": epoch_acc,
+        "recall": epoch_recall,
+        "f2": epoch_f2,
     }
